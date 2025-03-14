f8e7ccff9f06154e6fa64918f325a401cb932a7e
louis.he <jyxiaohe@gmail.com>
2025-03-14 23:41:00 +0800

    mm

--- change_static_ip.sh
+++ change_static_ip.sh
@@ -3,7 +3,7 @@
 # 配置静态 IP 地址
 STATIC_IP="192.168.239.128"  # 修改为你的静态 IP
 GATEWAY="192.168.239.1"      # 修改为你的网关
-DNS="8.8.8.8,8.8.4.4"      # Google DNS，可根据需要更改
+DNS="8.8.8.8 8.8.4.4"      # Google DNS，可根据需要更改
 INTERFACE="ens33"          # 你的网络接口，使用 `ip a` 检查
 
 # 备份原配置
@@ -14,10 +14,10 @@ echo "network:
   version: 2
   ethernets:
     $INTERFACE:
-      addresses: $STATIC_IP/24
+      addresses: [${STATIC_IP}/24]
       gateway4: $GATEWAY
       nameservers:
-        addresses: [$DNS]
+        addresses: [$(echo ${DNS} | sed 's/ /, /g')]
       dhcp4: no" | sudo tee /etc/netplan/50-cloud-init.yaml
 
 # 应用网络配置
